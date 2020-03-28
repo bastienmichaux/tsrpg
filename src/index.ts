4090ecf73107fbb04c9b35651992ea89b2e6a936
bmichaux@altissia.com <bmichaux@altissia.com>
2020-03-28 15:10:52 +0100

    add start screen

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,54 @@
-const app = document.getElementById('app');
-app.textContent = 'it works';
+interface TemplateP {
+  txt: string;
+}
+
+interface TemplateButton {
+  txt: string;
+  click: () => any;
+}
+
+class App {
+  static clear(): void {
+    const app = document.getElementById('app');
+    while (app.firstChild) app.removeChild(app.firstChild);
+  }
+  static main(): void {
+    App.clear();
+    StartScreen.render();
+  }
+}
+
+class Dom {
+  static button(tpl: TemplateButton): HTMLElement {
+    const button = document.createElement('button');
+    button.textContent = tpl.txt;
+    button.addEventListener('click', function() {
+      tpl.click();
+    });
+    return button;
+  }
+  static p(tpl: TemplateP): HTMLElement {
+    const p = document.createElement('p');
+    p.textContent = tpl.txt;
+    return p;
+  }
+}
+
+class StartScreen {
+  private static newGame(): void {
+    console.log('lol');
+  }
+  static render(): void {
+    App.clear();
+    const app = document.getElementById('app');
+    const title = Dom.p({txt: 'Rpg'});
+    const newGameBtn = Dom.button({
+      txt: 'New game',
+      click: StartScreen.newGame
+    });
+    app.appendChild(title);
+    app.appendChild(newGameBtn);
+  }
+}
+
+App.main();
